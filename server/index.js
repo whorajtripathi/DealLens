ff9cc1ceec600e3ba9d9a42271b3ad4977097d4c
whorajtripathi <rajsanjaykumartripathi@gmail.com>
2026-07-09 00:36:11 +0530

    Added vercel!!

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const port=process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://your-vercel-app.vercel.app" // Replace later
+  "https://deal-lens-two.vercel.app/" // vercel url
 ];
 
 app.use(cors({
